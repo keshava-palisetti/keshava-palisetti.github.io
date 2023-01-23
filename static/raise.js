492d516ddec19b48769d071b990aacf84c8614a2
keshava-palisetti <keshava974@gmail.com>
2023-01-23 14:47:41 +0530

    changed

---
+++ static/raise.js
@@ -0,0 +1,4 @@
+var outer_div = document.getElementById("outer-div");
+var register_div = document.getElementById("register-div");
+document.getElementById("register-div").style.translate = "-50% 0 0";
+document.getElementById("submit").style.translate = "-50% 0 0";
